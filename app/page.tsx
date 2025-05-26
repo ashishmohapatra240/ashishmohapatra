8cca10a2159c1892ba87416a30ac393eaf61518e
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-05-26 20:57:10 +0530

    feat: Added Works section into Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import { Hero } from "@/app/sections/Hero";
+import { Works } from "@/app/sections/Works";
+
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Works />
     </main>
   );
 }
