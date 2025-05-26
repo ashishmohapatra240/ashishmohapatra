0e7ce37546cac0adc84aad3ac095c08d631eb45a
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-05-27 00:10:02 +0530

    feat: Update metadata and enhance Home page layout with additional sections including Hackathons, Skills, Hall of Fame, Testimonials, and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import { Hero } from "@/app/sections/Hero";
 import { Works } from "@/app/sections/Works";
+import { Hackathons } from "@/app/sections/Hackathons";
+import { Skills } from "./sections/Skills";
+import { HallOfFame } from "@/app/sections/HallOfFame";
+import { Testimonials } from "@/app/sections/Testimonials";
+import { Footer } from "@/components/ui/footer";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-white">
       <Hero />
       <Works />
+      <Hackathons />
+      <Skills />
+      <HallOfFame />
+      <Testimonials />
+      <Footer />
     </main>
   );
 }
