19016c54c8db28eee49d4aac4e260eaf7f76a129
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-05-26 19:19:30 +0530

    feat: Add Hero section in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { Hero } from "@/app/sections/Hero";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main>
+      <Hero />
+    </main>
   );
 }
